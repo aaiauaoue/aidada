544e2653dff20bbb3437ed534894c752faf4b4b0
aaiauaoue <124838248+aaiauaoue@users.noreply.github.com>
2025-06-07 22:50:24 +0200

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-function sigHej() {
-  alert("Hej fra JavaScript!");
+function visKontakt() {
+  const kontakt = document.getElementById("kontaktinfo");
+  kontakt.style.display = kontakt.style.display === "none" ? "block" : "none";
 }
